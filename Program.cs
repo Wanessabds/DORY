4cea0fb08938543510a706bd18f56190c8d925bd
Wanessabds <wanessa.nessap@gmail.com>
2023-03-31 22:25:44 -0300

    DORY

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("___imitando dory___\n");
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.Write("Escreva uma frase___: ");
+string frase = Console.ReadLine()!;
+
+string fraseEmBaleies = frase
+    .Replace("a", "aaaa")
+    .Replace("A", "AAAA")
+    .Replace("e", "eeee")
+    .Replace("E", "EEEE")
+    .Replace("i", "iiii")
+    .Replace("I", "IIII")
+    .Replace("o", "oooo")
+    .Replace("O", "OOOO")
+    .Replace("u", "uuuu")
+    .Replace("U", "UUUU")
+;
+
+Console.WriteLine($"\nEm baleiês:\n\n{fraseEmBaleies}");
